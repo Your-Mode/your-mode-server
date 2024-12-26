8172a535bd75c7290ef12e2987b463d7e2ed612d
shimfff <shimseawon1510@gmail.com>
2024-12-26 21:02:22 +0900

    :sparkles: feat: 업주 권한 설정

---
+++ src/main/java/com/example/yourmode/domain/business/controller/BusinessOwnerController.java
@@ -0,0 +1,21 @@
+package com.example.yourmode.domain.business.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Tag(name = "업주용 업체 API", description = "업주용 업체 관련 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/owner/businesses")
+public class BusinessOwnerController {
+
+    // 업장 등록
+
+    // 업장 수정
+
+    // 업장 삭제
+
+}
---
+++ src/main/java/com/example/yourmode/domain/customer/controller/CustomerOwnerController.java
@@ -0,0 +1,22 @@
+package com.example.yourmode.domain.customer.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "업주용 고객 API", description = "업주용 고객 관련 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/owner/customers")
+public class CustomerOwnerController {
+    // 고객 등록
+
+    // 고객 수정
+
+    // 고객 삭제
+
+    // 고객 리스트 조회
+
+    // 고객 정보 상세 조회
+}
