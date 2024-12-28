8367c6daa1efdb306637d7b0d58dbb5a966c5ab6
shimfff <shimseawon1510@gmail.com>
2024-12-28 22:11:47 +0900

    :sparkles: feat: 업장 API 개발
    Related to: #3

--- src/main/java/com/example/yourmode/domain/business/repository/BusinessRepository.java
+++ src/main/java/com/example/yourmode/domain/business/repository/BusinessRepository.java
@@ -3,5 +3,6 @@ package com.example.yourmode.domain.business.repository;
 import com.example.yourmode.domain.business.entity.Business;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface BusinessRepository extends JpaRepository<Business, Long> {
 }
